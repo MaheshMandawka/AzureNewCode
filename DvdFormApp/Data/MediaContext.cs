460c075a5c5c75dcbf214f1beb8fb47fd6bf868a
Matthew Mackay <matthew.mackay@centralsquare.com>
2023-11-23 20:06:39 -0800

    Initial Database additions.

---
+++ DvdFormApp/Data/MediaContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MediaContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured == false)
+        {
+            optionsBuilder.UseSqlServer(
+                "server=.;Database=Media;trusted_connection=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    public MediaContext(DbContextOptions options)
+        : base(options) { }
+
+    public MediaContext()
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ DvdFormApp/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime? Date { get; set; }
+    public DateTime LastModified { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string Description { get; set; }
+    public int? BookshelfId { get; set; }
+    public string Type { get; set; }
+}
